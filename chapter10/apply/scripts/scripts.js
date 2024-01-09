8151b455cd315300a77e059eaf31bc2e2800d931
noahrxbinson <148370145+noahrxbinson@users.noreply.github.com>
2024-01-09 13:52:53 -0500

    Add files via upload

---
+++ chapter10/apply/scripts/scripts.js
@@ -0,0 +1,8 @@
+/*Student name: noah robinson Date: 1/8/2024*/
+function content() {
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "c0145f";
+    text.style.fontSize = "2em";
+    
+}
