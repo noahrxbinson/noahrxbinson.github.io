c9bb3705f2f855f26f36daf97ee8ead6ecdefc6f
noahrxbinson <148370145+noahrxbinson@users.noreply.github.com>
2024-04-10 13:27:18 -0400

    Add files via upload

---
+++ project02/project01-02.js
@@ -0,0 +1,22 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Noah Robinson
+    Date: 4/9/2024
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speedxd
+
+
+
+var service1Name = "Basic";
+var service2Name = "Express";
+var service3Name = "Extreme";
+var service4Name = "Ultimate";
+var service1Speed = "0 Mbps";
+var service2Speed = "100 Mbps";
+var service3Speed = "500 Mbps";
+var service4Speed = "1 Gig";
